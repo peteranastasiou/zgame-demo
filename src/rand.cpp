d6e2c5e12a4f2ea97e52a62dc6cd3c7e5ab74ebe
Peter Anastasiou <8896307+peteranastasiou@users.noreply.github.com>
2022-05-10 07:51:49 +0930

    random roll dice

--- src/rand.cpp
+++ src/rand.cpp
@@ -1,8 +1,18 @@
 
 #include "rand.hpp"
 
-uint32_t rand(uint32_t & seed){
+uint32_t rand(uint32_t & seed) {
     // Factors from Borland C rand impl
     seed = (22695477 * seed + 1);
     return seed;
+}
+
+uint32_t roll(uint32_t & seed, uint32_t n, uint32_t sides) {
+    uint32_t sum= 0;
+    for( uint32_t i = 0; i < n; ++i ){
+        uint32_t r = rand(seed);
+        uint32_t d = r % sides + 1;
+        sum += d;
+    }
+    return sum;
 }
--- src/rand.hpp
+++ src/rand.hpp
@@ -7,3 +7,9 @@
  * updates seed in place and also returns it for convenience
  */
 extern uint32_t rand(uint32_t & seed);
+
+/**
+ * roll <n>d<sides> e.g. 2d6
+ */
+extern uint32_t roll(uint32_t & seed, uint32_t n, uint32_t sides);
+
